c0cd131fd8cc4e8a1dde2283fa3f8ec8eedf2e83
raddaouiameni <raddaouiameni59@gmail.com>
2023-03-31 13:30:47 +0100

    fix conflict

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -24,47 +24,13 @@ def write_file_content(file_path, content,request, output_dir=None ):
         logging.debug(f"Wrote content to file: {chatgpt_file_path}")  # Log the write operation
 
  
-<<<<<<< HEAD
-def get_file_list(dir_path):
-=======
 
 def get_file_list(dir_path , request):
 
->>>>>>> b83ae6480ac2
     """Yields (root, dirs, files) tuples for all files in the directory that are not ignored by .gitignore. 
     Get all the files with specific extensions defined in the 'files_to_be_included' config."""
     gitignore_path = os.path.join(dir_path, ".gitignore")
 
-<<<<<<< HEAD
-    config = read_config()
-    requests = config["requests"]
-
-    for request in requests:
-        files_to_be_included = request.get("files_to_be_included", "*")
-        if files_to_be_included != "*":
-            patterns = files_to_be_included.split(",")
-            extensions = [os.path.splitext(pattern.strip())[1] for pattern in patterns]
-            logging.warning(f"Only {', '.join(extensions)} files will be analyzed")
-        
-        if os.path.exists(gitignore_path):
-            with open(gitignore_path, "r") as f:
-                ignore_patterns = f.read().splitlines()
-            for root, dirs, files in os.walk(dir_path):
-                if files_to_be_included != "*":
-                    files = [f for f in files if all(not fnmatch.fnmatch(f, pattern) for pattern in ignore_patterns) and any(f.endswith(ext) for ext in extensions)]
-                else:
-                    files = [f for f in files if all(not fnmatch.fnmatch(f, pattern) for pattern in ignore_patterns)] 
-                dirs[:] = [d for d in dirs if not any(fnmatch.fnmatch(d, pattern) for pattern in ignore_patterns)]
-                logging.debug(f"Processing directory: {root}")  # Log the directory being processed
-                yield root, dirs, files
-        else:
-            if files_to_be_included != "*":
-                files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))  and any(f.endswith(ext) for ext in extensions)]
-            else:
-                files = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f)) ]  
-            logging.debug(f"Processing directory: {dir_path}")  # Log the directory being processed
-            yield dir_path, [], files
-=======
     files_to_be_excluded = request["files_to_be_excluded"]
     exclude_patterns = files_to_be_excluded.split(",")
     extensions_excluded = [os.path.splitext(pattern.strip())[1] for pattern in exclude_patterns]
@@ -101,5 +67,3 @@ def get_file_list(dir_path , request):
 
 
     
-    
->>>>>>> b83ae6480ac2
